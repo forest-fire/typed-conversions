a4e2533906dd4570777f81716fc5fd645b6cf406
Ken Snyder <ken@ken.net>
2017-08-09 17:33:54 -0700

    0.6.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,3 +5,4 @@ export declare function flatten(list: any[]): any[];
 export declare function arrayToHash<T = IDictionary>(list: any[], __key__?: string): any;
 export declare function snapshotToArray<T = IDictionary>(snap: Firebase.database.DataSnapshot, idProp?: string): T[];
 export declare function snapshotToOrderedArray<T = IDictionary>(snap: Firebase.database.DataSnapshot, idProp?: string): T[];
+export declare function snapshotToOrderedHash<T = IDictionary>(snap: Firebase.database.DataSnapshot, idProp?: string): IDictionary<T>;
