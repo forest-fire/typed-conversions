2423d6991e70cc2e0f5b46e7754a953494e40bed
Ken Snyder <ken@ken.net>
2019-03-12 00:09:36 -0700

    transpiled for node 8

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,15 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 
-var lodashEs = require("lodash-es");
+var lodashEs = require('lodash-es');
 
-function removeIdPropertyFromHash(hash) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var output = {};
-  Object.keys(hash).map(function(objId) {
-    var input = hash[objId];
+function removeIdPropertyFromHash(hash, idProp = "id") {
+  const output = {};
+  Object.keys(hash).map(objId => {
+    const input = hash[objId];
     output[objId] = {};
-    Object.keys(input).map(function(prop) {
+    Object.keys(input).map(prop => {
       if (prop !== idProp) {
         output[objId][prop] = input[prop];
       }
@@ -18,91 +17,62 @@ function removeIdPropertyFromHash(hash) {
   });
   return output;
 }
-function keyValueDictionaryToArray(dict) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+function keyValueDictionaryToArray(dict, options = {}) {
+  const __key__ = options.key || "id";
 
-  var __key__ = options.key || "id";
+  const __value__ = options.value || "value";
 
-  var __value__ = options.value || "value";
-
-  return Object.keys(dict).reduce(function(result, key) {
-    var _result$concat;
-
-    return result.concat(
-      ((_result$concat = {}),
-      _rollupPluginBabelHelpers_js.defineProperty(_result$concat, __key__, key),
-      _rollupPluginBabelHelpers_js.defineProperty(_result$concat, __value__, dict[key]),
-      _result$concat)
-    );
+  return Object.keys(dict).reduce((result, key) => {
+    return result.concat({
+      [__key__]: key,
+      [__value__]: dict[key]
+    });
   }, []);
 }
-function keyValueArrayToDictionary(input) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+function keyValueArrayToDictionary(input, options = {}) {
+  const __key__ = options.key || "key";
 
-  var __key__ = options.key || "key";
+  const __value__ = options.value || "value";
 
-  var __value__ = options.value || "value";
-
-  return input.reduce(function(output, curr) {
-    var key = curr[__key__];
-    var value = curr[__value__];
+  return input.reduce((output, curr) => {
+    const key = curr[__key__];
+    const value = curr[__value__];
     output[key] = value;
     return output;
   }, {});
 }
-function hashToArray(hashObj) {
-  var __key__ = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-
-  if (hashObj && _rollupPluginBabelHelpers_js.typeof(hashObj) !== "object") {
-    throw new Error(
-      "Cant convert hash-to-array because hash was not passed in: " + hashObj
-    );
+function hashToArray(hashObj, __key__ = "id") {
+  if (hashObj && typeof hashObj !== "object") {
+    throw new Error("Cant convert hash-to-array because hash was not passed in: " + hashObj);
   }
 
-  var hash = Object.assign({}, hashObj);
-  var results = [];
-  var isHashArray = Object.keys(hash).every(function(i) {
-    return hash[i] === true;
-  });
-  var isHashValue = Object.keys(hash).every(function(i) {
-    return _rollupPluginBabelHelpers_js.typeof(hash[i]) !== "object";
-  });
-  Object.keys(hash).map(function(id) {
-    var _ref;
-
-    var obj =
-      _rollupPluginBabelHelpers_js.typeof(hash[id]) === "object"
-        ? Object.assign(
-            {},
-            hash[id],
-            _rollupPluginBabelHelpers_js.defineProperty({}, __key__, id)
-          )
-        : isHashArray
-        ? id
-        : ((_ref = {}),
-          _rollupPluginBabelHelpers_js.defineProperty(_ref, __key__, id),
-          _rollupPluginBabelHelpers_js.defineProperty(_ref, "value", hash[id]),
-          _ref);
+  const hash = Object.assign({}, hashObj);
+  const results = [];
+  const isHashArray = Object.keys(hash).every(i => hash[i] === true);
+  const isHashValue = Object.keys(hash).every(i => typeof hash[i] !== "object");
+  Object.keys(hash).map(id => {
+    const obj = typeof hash[id] === "object" ? Object.assign({}, hash[id], {
+      [__key__]: id
+    }) : isHashArray ? id : {
+      [__key__]: id,
+      value: hash[id]
+    };
     results.push(obj);
   });
   return results;
 }
 function flatten(list) {
-  return list.reduce(function(a, b) {
-    return a.concat(Array.isArray(b) ? flatten(b) : b);
-  }, []);
+  return list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 }
 function arrayToHash(arr, keyProperty) {
   if (arr.length === 0) {
     return {};
   }
 
-  var isScalar = _rollupPluginBabelHelpers_js.typeof(arr[0]) === "object" ? false : true;
+  const isScalar = typeof arr[0] === "object" ? false : true;
 
   if (isScalar && keyProperty) {
-    var e = new Error(
-      "You can not have an array of primitive values AND set a keyProperty!"
-    );
+    const e = new Error(`You can not have an array of primitive values AND set a keyProperty!`);
     e.name = "NotAllowed";
     throw e;
   }
@@ -111,91 +81,65 @@ function arrayToHash(arr, keyProperty) {
     if (arr[0].hasOwnProperty("id")) {
       keyProperty = "id";
     } else {
-      var _e = new Error(
-        'Tried to default to a keyProperty of "id" but that property does not appear to be in the array passed in'
-      );
-
-      _e.name = "NotAllowed";
-      throw _e;
+      const e = new Error(`Tried to default to a keyProperty of "id" but that property does not appear to be in the array passed in`);
+      e.name = "NotAllowed";
+      throw e;
     }
   }
 
   if (!Array.isArray(arr)) {
-    var _e2 = new Error("arrayToHash: input was not an array!");
-
-    _e2.name = "NotAllowed";
-    throw _e2;
+    const e = new Error(`arrayToHash: input was not an array!`);
+    e.name = "NotAllowed";
+    throw e;
   }
 
-  var output = arr.reduce(function(prev, curr) {
-    var key = isScalar
-      ? curr
-      : typeof keyProperty === "function"
-      ? keyProperty(curr)
-      : curr[keyProperty];
-    return isScalar
-      ? Object.assign(
-          {},
-          prev,
-          _rollupPluginBabelHelpers_js.defineProperty({}, key, true)
-        )
-      : Object.assign(
-          {},
-          prev,
-          _rollupPluginBabelHelpers_js.defineProperty({}, key, curr)
-        );
+  const output = arr.reduce((prev, curr) => {
+    const key = isScalar ? curr : typeof keyProperty === "function" ? keyProperty(curr) : curr[keyProperty];
+    return isScalar ? Object.assign({}, prev, {
+      [key]: true
+    }) : Object.assign({}, prev, {
+      [key]: curr
+    });
   }, {});
   return output;
 }
-function snapshotToArray(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var hash = snap.val() || {};
+function snapshotToArray(snap, idProp = "id") {
+  const hash = snap.val() || {};
   return hashToArray(hash, idProp);
 }
-function snapshotToHash(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var hash = snap.val() || {};
-  Object.keys(hash).forEach(function(key) {
-    var _hash$key;
-
-    return _rollupPluginBabelHelpers_js.typeof(hash[key]) === "object"
-      ? (hash[key][idProp] = key)
-      : (hash[key] = ((_hash$key = {}),
-        _rollupPluginBabelHelpers_js.defineProperty(_hash$key, idProp, key),
-        _rollupPluginBabelHelpers_js.defineProperty(_hash$key, "value", hash[key]),
-        _hash$key));
+function snapshotToHash(snap, idProp = "id") {
+  const hash = snap.val() || {};
+  Object.keys(hash).forEach(key => typeof hash[key] === "object" ? hash[key][idProp] = key : hash[key] = {
+    [idProp]: key,
+    value: hash[key]
   });
   return hash;
 }
-function snapshotToOrderedArray(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var output = [];
-  snap.forEach(function(child) {
-    var obj = child.val();
-    var key = child.key;
+function snapshotToOrderedArray(snap, idProp = "id") {
+  const output = [];
+  snap.forEach(child => {
+    const obj = child.val();
+    const key = child.key;
 
-    if (_rollupPluginBabelHelpers_js.typeof(obj) !== "object") {
-      throw new Error(
-        "Can't create a list from scalar values: \"".concat(obj, '" | "').concat(key, '"')
-      );
+    if (typeof obj !== "object") {
+      throw new Error(`Can't create a list from scalar values: "${obj}" | "${key}"`);
     }
 
-    output.push(
-      Object.assign(_rollupPluginBabelHelpers_js.defineProperty({}, idProp, key), obj)
-    );
+    output.push(Object.assign({
+      [idProp]: key
+    }, obj));
     return true;
   });
   return output;
 }
-function snapshotToOrderedHash(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var orderedArray = this.snapshotToOrderedArray(snap, idProp);
+function snapshotToOrderedHash(snap, idProp = "id") {
+  const orderedArray = this.snapshotToOrderedArray(snap, idProp);
   return this.arrayToHash(orderedArray);
 }
 function getPropertyAcrossDictionaryItems(dictionary, property) {
-  var output = [];
-  Object.keys(dictionary).map(function(item) {
-    var value = lodashEs.get(dictionary[item], property, undefined);
+  const output = [];
+  Object.keys(dictionary).map(item => {
+    const value = lodashEs.get(dictionary[item], property, undefined);
 
     if (value !== undefined) {
       output.push(value);
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,12 +1,11 @@
-import { get } from "lodash-es";
+import { get } from 'lodash-es';
 
-function removeIdPropertyFromHash(hash) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var output = {};
-  Object.keys(hash).map(function(objId) {
-    var input = hash[objId];
+function removeIdPropertyFromHash(hash, idProp = "id") {
+  const output = {};
+  Object.keys(hash).map(objId => {
+    const input = hash[objId];
     output[objId] = {};
-    Object.keys(input).map(function(prop) {
+    Object.keys(input).map(prop => {
       if (prop !== idProp) {
         output[objId][prop] = input[prop];
       }
@@ -14,87 +13,62 @@ function removeIdPropertyFromHash(hash) {
   });
   return output;
 }
-function keyValueDictionaryToArray(dict) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+function keyValueDictionaryToArray(dict, options = {}) {
+  const __key__ = options.key || "id";
 
-  var __key__ = options.key || "id";
+  const __value__ = options.value || "value";
 
-  var __value__ = options.value || "value";
-
-  return Object.keys(dict).reduce(function(result, key) {
-    var _result$concat;
-
-    return result.concat(
-      ((_result$concat = {}),
-      defineProperty(_result$concat, __key__, key),
-      defineProperty(_result$concat, __value__, dict[key]),
-      _result$concat)
-    );
+  return Object.keys(dict).reduce((result, key) => {
+    return result.concat({
+      [__key__]: key,
+      [__value__]: dict[key]
+    });
   }, []);
 }
-function keyValueArrayToDictionary(input) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+function keyValueArrayToDictionary(input, options = {}) {
+  const __key__ = options.key || "key";
 
-  var __key__ = options.key || "key";
+  const __value__ = options.value || "value";
 
-  var __value__ = options.value || "value";
-
-  return input.reduce(function(output, curr) {
-    var key = curr[__key__];
-    var value = curr[__value__];
+  return input.reduce((output, curr) => {
+    const key = curr[__key__];
+    const value = curr[__value__];
     output[key] = value;
     return output;
   }, {});
 }
-function hashToArray(hashObj) {
-  var __key__ = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-
-  if (hashObj && typeof$1(hashObj) !== "object") {
-    throw new Error(
-      "Cant convert hash-to-array because hash was not passed in: " + hashObj
-    );
+function hashToArray(hashObj, __key__ = "id") {
+  if (hashObj && typeof hashObj !== "object") {
+    throw new Error("Cant convert hash-to-array because hash was not passed in: " + hashObj);
   }
 
-  var hash = Object.assign({}, hashObj);
-  var results = [];
-  var isHashArray = Object.keys(hash).every(function(i) {
-    return hash[i] === true;
-  });
-  var isHashValue = Object.keys(hash).every(function(i) {
-    return typeof$1(hash[i]) !== "object";
-  });
-  Object.keys(hash).map(function(id) {
-    var _ref;
-
-    var obj =
-      typeof$1(hash[id]) === "object"
-        ? Object.assign({}, hash[id], defineProperty({}, __key__, id))
-        : isHashArray
-        ? id
-        : ((_ref = {}),
-          defineProperty(_ref, __key__, id),
-          defineProperty(_ref, "value", hash[id]),
-          _ref);
+  const hash = Object.assign({}, hashObj);
+  const results = [];
+  const isHashArray = Object.keys(hash).every(i => hash[i] === true);
+  const isHashValue = Object.keys(hash).every(i => typeof hash[i] !== "object");
+  Object.keys(hash).map(id => {
+    const obj = typeof hash[id] === "object" ? Object.assign({}, hash[id], {
+      [__key__]: id
+    }) : isHashArray ? id : {
+      [__key__]: id,
+      value: hash[id]
+    };
     results.push(obj);
   });
   return results;
 }
 function flatten(list) {
-  return list.reduce(function(a, b) {
-    return a.concat(Array.isArray(b) ? flatten(b) : b);
-  }, []);
+  return list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 }
 function arrayToHash(arr, keyProperty) {
   if (arr.length === 0) {
     return {};
   }
 
-  var isScalar = typeof$1(arr[0]) === "object" ? false : true;
+  const isScalar = typeof arr[0] === "object" ? false : true;
 
   if (isScalar && keyProperty) {
-    var e = new Error(
-      "You can not have an array of primitive values AND set a keyProperty!"
-    );
+    const e = new Error(`You can not have an array of primitive values AND set a keyProperty!`);
     e.name = "NotAllowed";
     throw e;
   }
@@ -103,81 +77,65 @@ function arrayToHash(arr, keyProperty) {
     if (arr[0].hasOwnProperty("id")) {
       keyProperty = "id";
     } else {
-      var _e = new Error(
-        'Tried to default to a keyProperty of "id" but that property does not appear to be in the array passed in'
-      );
-
-      _e.name = "NotAllowed";
-      throw _e;
+      const e = new Error(`Tried to default to a keyProperty of "id" but that property does not appear to be in the array passed in`);
+      e.name = "NotAllowed";
+      throw e;
     }
   }
 
   if (!Array.isArray(arr)) {
-    var _e2 = new Error("arrayToHash: input was not an array!");
-
-    _e2.name = "NotAllowed";
-    throw _e2;
+    const e = new Error(`arrayToHash: input was not an array!`);
+    e.name = "NotAllowed";
+    throw e;
   }
 
-  var output = arr.reduce(function(prev, curr) {
-    var key = isScalar
-      ? curr
-      : typeof keyProperty === "function"
-      ? keyProperty(curr)
-      : curr[keyProperty];
-    return isScalar
-      ? Object.assign({}, prev, defineProperty({}, key, true))
-      : Object.assign({}, prev, defineProperty({}, key, curr));
+  const output = arr.reduce((prev, curr) => {
+    const key = isScalar ? curr : typeof keyProperty === "function" ? keyProperty(curr) : curr[keyProperty];
+    return isScalar ? Object.assign({}, prev, {
+      [key]: true
+    }) : Object.assign({}, prev, {
+      [key]: curr
+    });
   }, {});
   return output;
 }
-function snapshotToArray(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var hash = snap.val() || {};
+function snapshotToArray(snap, idProp = "id") {
+  const hash = snap.val() || {};
   return hashToArray(hash, idProp);
 }
-function snapshotToHash(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var hash = snap.val() || {};
-  Object.keys(hash).forEach(function(key) {
-    var _hash$key;
-
-    return typeof$1(hash[key]) === "object"
-      ? (hash[key][idProp] = key)
-      : (hash[key] = ((_hash$key = {}),
-        defineProperty(_hash$key, idProp, key),
-        defineProperty(_hash$key, "value", hash[key]),
-        _hash$key));
+function snapshotToHash(snap, idProp = "id") {
+  const hash = snap.val() || {};
+  Object.keys(hash).forEach(key => typeof hash[key] === "object" ? hash[key][idProp] = key : hash[key] = {
+    [idProp]: key,
+    value: hash[key]
   });
   return hash;
 }
-function snapshotToOrderedArray(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var output = [];
-  snap.forEach(function(child) {
-    var obj = child.val();
-    var key = child.key;
+function snapshotToOrderedArray(snap, idProp = "id") {
+  const output = [];
+  snap.forEach(child => {
+    const obj = child.val();
+    const key = child.key;
 
-    if (typeof$1(obj) !== "object") {
-      throw new Error(
-        "Can't create a list from scalar values: \"".concat(obj, '" | "').concat(key, '"')
-      );
+    if (typeof obj !== "object") {
+      throw new Error(`Can't create a list from scalar values: "${obj}" | "${key}"`);
     }
 
-    output.push(Object.assign(defineProperty({}, idProp, key), obj));
+    output.push(Object.assign({
+      [idProp]: key
+    }, obj));
     return true;
   });
   return output;
 }
-function snapshotToOrderedHash(snap) {
-  var idProp = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "id";
-  var orderedArray = this.snapshotToOrderedArray(snap, idProp);
+function snapshotToOrderedHash(snap, idProp = "id") {
+  const orderedArray = this.snapshotToOrderedArray(snap, idProp);
   return this.arrayToHash(orderedArray);
 }
 function getPropertyAcrossDictionaryItems(dictionary, property) {
-  var output = [];
-  Object.keys(dictionary).map(function(item) {
-    var value = get(dictionary[item], property, undefined);
+  const output = [];
+  Object.keys(dictionary).map(item => {
+    const value = get(dictionary[item], property, undefined);
 
     if (value !== undefined) {
       output.push(value);
@@ -186,16 +144,4 @@ function getPropertyAcrossDictionaryItems(dictionary, property) {
   return output;
 }
 
-export {
-  removeIdPropertyFromHash,
-  keyValueDictionaryToArray,
-  keyValueArrayToDictionary,
-  hashToArray,
-  flatten,
-  arrayToHash,
-  snapshotToArray,
-  snapshotToHash,
-  snapshotToOrderedArray,
-  snapshotToOrderedHash,
-  getPropertyAcrossDictionaryItems
-};
+export { removeIdPropertyFromHash, keyValueDictionaryToArray, keyValueArrayToDictionary, hashToArray, flatten, arrayToHash, snapshotToArray, snapshotToHash, snapshotToOrderedArray, snapshotToOrderedHash, getPropertyAcrossDictionaryItems };
