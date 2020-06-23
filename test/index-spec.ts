d36526bcb4fe4bc2732f9f52903ffebb0a6aa294
Ken Snyder <ken@ken.net>
2020-06-23 11:25:54 -0700

    jest tests working

--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,9 +1,10 @@
 import convert = require("../src");
 
+import "jest-extended";
+
 import * as helpers from "./testing/helpers";
 
 import { INameValuePair } from "common-types";
-import { expect } from "chai";
 import { removeIdPropertyFromHash } from "../src/index";
 
 const basicHash = {
@@ -92,68 +93,76 @@ describe("snapshotToHash", () => {
   it("works with default idProp", () => {
     const converted = convert.snapshotToHash(listSnapshot as any);
 
-    expect(converted).to.be.an("object");
-    expect(helpers.length(converted)).to.equal(8);
-    expect(helpers.firstRecord(converted)).has.property("id");
+    expect(converted).toBeObject();
+    expect(helpers.length(converted)).toBe(8);
+    expect(helpers.firstRecord(converted)).toHaveProperty("id");
   });
 
   it("works with bespoke idProp", () => {
     const converted = convert.snapshotToHash(listSnapshot as any, "key");
 
-    expect(converted).to.be.an("object");
-    expect(helpers.length(converted)).to.equal(8);
-    expect(helpers.firstRecord(converted)).has.property("key");
+    expect(converted).toBeObject();
+    expect(helpers.length(converted)).toBe(8);
+    expect(helpers.firstRecord(converted)).toHaveProperty("key");
   });
 
   it("works with a scalar snapshot", () => {
     const converted = convert.snapshotToHash(scalarSnapshot as any, "key");
 
-    expect(converted).to.be.an("object");
-    expect(helpers.length(converted)).to.equal(8);
-    expect(helpers.firstRecord(converted)).has.property("key");
+    expect(converted).toBeObject();
+    expect(helpers.length(converted)).toBe(8);
+    expect(helpers.firstRecord(converted)).toHaveProperty("key");
   });
 });
 
 describe("hashToArray()", () => {
   it("works with no 'id' conflicts", () => {
     const converted = convert.hashToArray({ ...{}, ...basicHash });
-    expect(converted).to.be.an("array");
-    expect(converted.length).to.equal(Object.keys(basicHash).length);
-    expect(converted.filter((i) => i.age === 22).length).to.equal(2);
+    expect(converted).toBeArray();
+    expect(converted.length).toBe(Object.keys(basicHash).length);
+    expect(converted.filter((i) => i.age === 22).length).toBe(2);
   });
 
   it("works with an 'id' conflict", () => {
     const converted = convert.hashToArray(conflictedHash);
 
-    expect(converted).to.be.an("array");
-    expect(converted.length).to.equal(Object.keys(conflictedHash).length);
-    expect(converted.filter((i) => i.age === 22).length).to.equal(3);
-    expect(converted.map((c: any) => c.id)).includes("-K13129djdf");
-    expect(converted.map((c: any) => c.id)).not.includes("1341");
+    expect(converted).toBeArray();
+    expect(converted.length).toBe(Object.keys(conflictedHash).length);
+    expect(converted.filter((i) => i.age === 22).length).toBe(3);
+    expect(converted.map((c: any) => c.id)).toEqual(
+      expect.arrayContaining(["-K13129djdf"])
+    );
+    expect(converted.map((c: any) => c.id)).toEqual(
+      expect.not.arrayContaining(["1341"])
+    );
   });
 
   it("works with an alternate ID property", () => {
     const converted = convert.hashToArray(conflictedHash, "_id" as any);
-    expect(converted).to.be.an("array");
-    expect(converted.length).to.equal(Object.keys(conflictedHash).length);
-    expect(converted.filter((i) => i.age === 22).length).to.equal(3);
-    expect(converted.map((c: any) => c._id)).includes("-K13129djdf");
-    expect(converted.map((c: any) => c._id)).not.includes("1341");
+    expect(converted).toBeArray();
+    expect(converted.length).toBe(Object.keys(conflictedHash).length);
+    expect(converted.filter((i) => i.age === 22).length).toBe(3);
+    expect(converted.map((c: any) => c._id)).toEqual(
+      expect.arrayContaining(["-K13129djdf"])
+    );
+    expect(converted.map((c: any) => c._id)).toEqual(
+      expect.not.arrayContaining(["1341"])
+    );
   });
 
   it("scalar name/value converts to array of name value", () => {
     const converted = convert.keyValueDictionaryToArray(scalarHash, {
       key: "key",
     });
-    expect(converted).to.be.an("array");
-    expect(converted.length).to.equal(Object.keys(scalarHash).length);
+    expect(converted).toBeArray();
+    expect(converted.length).toBe(Object.keys(scalarHash).length);
 
     converted.map((item) => {
-      expect(item).to.be.an("object");
-      expect(item).to.haveOwnProperty("key");
-      expect(item).to.haveOwnProperty("value");
+      expect(item).toBeObject();
+      expect(item).toHaveProperty("key");
+      expect(item).toHaveProperty("value");
 
-      expect(item.value).to.be.a("number");
+      expect(item.value).toBeNumber();
     });
   });
 
@@ -166,12 +175,13 @@ describe("hashToArray()", () => {
       "-LFsnvt2hq28zZHeddyn": 1529961496131,
     };
     const converted = convert.hashToArray(data);
-    expect(converted).to.be.an("array").and.to.have.lengthOf(5);
+    expect(converted).toBeArray();
+    expect(converted).toHaveLength(5);
 
     converted.forEach((c) => {
-      expect(c).to.be.an("object");
-      expect(c).to.haveOwnProperty("id");
-      expect(c).to.haveOwnProperty("value");
+      expect(c).toBeObject();
+      expect(c).toHaveProperty("id");
+      expect(c).toHaveProperty("value");
     });
   });
 
@@ -184,21 +194,22 @@ describe("hashToArray()", () => {
       "-LFsnvt2hq28zZHeddyn": true,
     };
     const converted = convert.hashToArray(data);
-    expect(converted).to.be.an("array").and.to.have.lengthOf(5);
-    expect(converted[0]).to.be.a("string");
+    expect(converted).toBeArray();
+    expect(converted).toHaveLength(5);
+    expect(converted[0]).toBeString();
   });
 
   it("scalar name/value converts to array of name value", () => {
     const converted = convert.keyValueDictionaryToArray(scalarHash());
-    expect(converted).to.be.an("array");
+    expect(converted).toBeArray();
 
-    expect(converted.length).to.equal(Object.keys(scalarHash()).length);
+    expect(converted.length).toBe(Object.keys(scalarHash()).length);
 
     converted.map((item) => {
-      expect(item).to.be.an("object");
-      expect(item).to.haveOwnProperty("id");
-      expect(item).to.haveOwnProperty("value");
-      expect(item.value).to.be.a("number");
+      expect(item).toBeObject();
+      expect(item).toHaveProperty("id");
+      expect(item).toHaveProperty("value");
+      expect(item.value).toBeNumber();
     });
   });
 
@@ -209,33 +220,31 @@ describe("hashToArray()", () => {
     const convertedBack = convert.keyValueArrayToDictionary(converted, {
       key: "id",
     });
-    expect(converted).to.be.an("array");
-    expect(convertedBack).to.be.an("object");
-    expect(JSON.stringify(scalarHash())).to.equal(
-      JSON.stringify(convertedBack)
-    );
+    expect(converted).toBeArray();
+    expect(convertedBack).toBeObject();
+    expect(JSON.stringify(scalarHash())).toBe(JSON.stringify(convertedBack));
   });
 
   it("scalar name/value pairing converts to an name/value dictionary", () => {
     const converted = convert.hashToArray<INameValuePair>(scalarHash());
-    expect(converted).to.be.an("array");
+    expect(converted).toBeArray();
 
-    expect(converted.length).to.equal(Object.keys(scalarHash()).length);
+    expect(converted.length).toBe(Object.keys(scalarHash()).length);
 
     converted.map((item) => {
-      expect(item).to.be.an("object");
-      expect(item).to.haveOwnProperty("id");
-      expect(item).to.haveOwnProperty("value");
-      expect(item.value).to.be.a("number");
+      expect(item).toBeObject();
+      expect(item).toHaveProperty("id");
+      expect(item).toHaveProperty("value");
+      expect(item.value).toBeNumber();
     });
   });
 
   it("scalar keys mapped to true returns a simple array", () => {
     const converted = convert.hashToArray(scalarArray());
-    expect(converted).to.be.an("array");
-    expect(converted.length).to.equal(Object.keys(scalarArray()).length);
+    expect(converted).toBeArray();
+    expect(converted.length).toBe(Object.keys(scalarArray()).length);
     Object.keys(scalarArray()).map((key) => {
-      expect(converted).to.contain(key);
+      expect(converted).toEqual(expect.arrayContaining([key]));
     });
   });
 });
@@ -252,9 +261,9 @@ describe("arrayToHash()", () => {
 
   it("works with default ID property", () => {
     const hash = convert.arrayToHash(simpleList);
-    expect(hash).to.be.an("object");
-    expect(helpers.length(hash)).is.equal(simpleList.length);
-    expect(helpers.firstRecord(hash).name).to.equal("Bob");
+    expect(hash).toBeObject();
+    expect(helpers.length(hash)).toBe(simpleList.length);
+    expect(helpers.firstRecord(hash).name).toBe("Bob");
   });
 
   it("works with bespoke ID property", () => {
@@ -266,27 +275,27 @@ describe("arrayToHash()", () => {
       }),
       "_id"
     );
-    expect(hash).to.be.an("object");
-    expect(helpers.length(hash)).is.equal(simpleList.length);
-    expect(helpers.firstRecord(hash).name).to.equal("Bob");
+    expect(hash).toBeObject();
+    expect(helpers.length(hash)).toBe(simpleList.length);
+    expect(helpers.firstRecord(hash).name).toBe("Bob");
   });
 
   it("works with an array of scalar values", () => {
     const converted = convert.arrayToHash(scalarList);
     // const reverted = convert.hashToArray(converted);
-    expect(converted).to.be.an("object");
-    expect(Object.keys(converted)).contains("foo");
-    expect(Object.keys(converted)).contains("bar");
-    expect(Object.keys(converted)).contains("baz");
-    expect(helpers.length(converted)).is.equal(scalarList.length);
+    expect(converted).toBeObject();
+    expect(Object.keys(converted)).toEqual(expect.arrayContaining(["foo"]));
+    expect(Object.keys(converted)).toEqual(expect.arrayContaining(["bar"]));
+    expect(Object.keys(converted)).toEqual(expect.arrayContaining(["baz"]));
+    expect(helpers.length(converted)).toBe(scalarList.length);
   });
 
   it("scalar array produces is idempotent with hashToArray()", async () => {
     const converted = convert.arrayToHash(scalarList);
     const reverted = convert.hashToArray(converted);
-    expect(reverted).to.have.lengthOf(Object.keys(converted).length);
+    expect(reverted).toHaveLength(Object.keys(converted).length);
     reverted.map((item) => {
-      expect(Object.keys(converted)).contains(item);
+      expect(Object.keys(converted)).toEqual(expect.arrayContaining([item]));
     });
   });
 
@@ -294,12 +303,12 @@ describe("arrayToHash()", () => {
     const converted = convert.hashToArray(basicHash); // brings in ID prop to array
     const reverted = removeIdPropertyFromHash(convert.arrayToHash(converted));
 
-    expect(JSON.stringify(reverted)).to.equal(JSON.stringify(basicHash));
+    expect(JSON.stringify(reverted)).toBe(JSON.stringify(basicHash));
 
     const c2 = convert.hashToArray(basicHashWithId);
     const r2 = convert.arrayToHash(c2);
 
-    expect(JSON.stringify(r2)).to.equal(JSON.stringify(basicHashWithId));
+    expect(JSON.stringify(r2)).toBe(JSON.stringify(basicHashWithId));
   });
 });
 
@@ -308,24 +317,24 @@ describe("snapshotToArray()", () => {
     const list = convert.snapshotToArray(listSnapshot as any);
     const expectedLength = helpers.length(listSnapshot.val());
 
-    expect(Object.keys(list).length).to.equal(expectedLength);
+    expect(Object.keys(list).length).toBe(expectedLength);
     const knownKey = helpers.firstKey(listSnapshot.val());
-    expect(list.map((i) => i.id)).to.include(knownKey);
-    expect(list[0].id).to.be.a("string");
-    expect(list[0].name).to.be.a("string");
-    expect(list[0].age).to.be.a("number");
+    expect(list.map((i) => i.id)).toEqual(expect.arrayContaining([knownKey]));
+    expect(list[0].id).toBeString();
+    expect(list[0].name).toBeString();
+    expect(list[0].age).toBeNumber();
   });
 
   it('snapshot with non-default "id" works', () => {
     const list = convert.snapshotToArray(listSnapshot as any, "_id");
     const expectedLength = helpers.length(listSnapshot.val());
 
-    expect(Object.keys(list).length).to.equal(expectedLength);
+    expect(Object.keys(list).length).toBe(expectedLength);
     const knownKey = helpers.firstKey(listSnapshot.val());
-    expect(list.map((i) => i._id)).to.include(knownKey);
-    expect(list[0]._id).to.be.a("string");
-    expect(list[0].name).to.be.a("string");
-    expect(list[0].age).to.be.a("number");
+    expect(list.map((i) => i._id)).toEqual(expect.arrayContaining([knownKey]));
+    expect(list[0]._id).toBeString();
+    expect(list[0].name).toBeString();
+    expect(list[0].age).toBeNumber();
   });
 
   // it("snapshot of a hash with scalar values creates id/value objects", () => {
@@ -340,15 +349,15 @@ describe("snapshotToArray()", () => {
     try {
       const scalar = convert.snapshotToArray(valueSnapshot as any);
     } catch (e) {
-      expect(true).to.equal(true);
+      expect(true).toBe(true);
     }
   });
 });
 
 describe("snapshotToSortedArray()", () => {
-  it.skip("key sort returns results in right order");
-  it.skip("child sort returns results in right order");
-  it.skip("value sort returns results in right order");
+  it.skip("key sort returns results in right order", () => undefined);
+  it.skip("child sort returns results in right order", () => undefined);
+  it.skip("value sort returns results in right order", () => undefined);
 });
 
 describe("flatten()", () => {
@@ -370,18 +379,24 @@ describe("flatten()", () => {
   ];
 
   it("flattening a multi-dimensional arrays which have intersection works", () => {
-    expect(convert.flatten(multiNumbersWithIntersection).length).to.equal(9);
-    expect(convert.flatten(multiNumbersWithIntersection)).to.include(1);
-    expect(convert.flatten(multiNumbersWithIntersection)).to.not.include(5);
-    expect(convert.flatten(multiNumbersWithIntersection)).to.include(9);
+    expect(convert.flatten(multiNumbersWithIntersection).length).toBe(9);
+    expect(convert.flatten(multiNumbersWithIntersection)).toEqual(
+      expect.arrayContaining([1])
+    );
+    expect(convert.flatten(multiNumbersWithIntersection)).toEqual(
+      expect.not.arrayContaining([5])
+    );
+    expect(convert.flatten(multiNumbersWithIntersection)).toEqual(
+      expect.arrayContaining([9])
+    );
     expect(
       convert.flatten(multiNumbersWithIntersection).filter((n) => n === 1)
         .length
-    ).to.equal(3);
+    ).toBe(3);
   });
 
   it("array of promises flattens correctly", () => {
-    expect(convert.flatten(arrayOfPromises).length).to.equal(6);
+    expect(convert.flatten(arrayOfPromises).length).toBe(6);
   });
 });
 
@@ -405,9 +420,9 @@ describe("others > ", () => {
       dictionary,
       "meta.thingy"
     );
-    expect(converted).to.be.an("array");
-    expect(converted).to.have.lengthOf(2);
-    expect(converted).to.contain(1);
-    expect(converted).to.contain(2);
+    expect(converted).toBeArray();
+    expect(converted).toHaveLength(2);
+    expect(converted).toEqual(expect.arrayContaining([1]));
+    expect(converted).toEqual(expect.arrayContaining([2]));
   });
 });
