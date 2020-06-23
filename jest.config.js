c7d931949ad54a7f5192e9941696f3f3d5a6f7f3
Ken Snyder <ken@ken.net>
2020-06-23 14:35:25 -0700

    wallaby configured; esModuleInterop/allowSyntheticDefaultImports set to FALSE;

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   testMatch: ["**/test/?(*-)+(spec|test).[jt]s?(x)"],
   // https://jestjs.io/docs/en/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
   transform: {
-    "lodash-es.+\\.js$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
   },
   // https://jestjs.io/docs/en/configuration#transformignorepatterns-arraystring
